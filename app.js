e5f19e134e7616d6b44749a63f67a2e903f696ef
annie <ac8502@nyu.edu>
2022-11-02 15:24:41 -0400

    part two done

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => {
-    res.send("Hello World!")
+    console.log(req);
+    res.send("Hello World!");
 });
+
+app.use('/images', express.static('public'));
 
 app.listen(port, () => console.log("Working!"));
